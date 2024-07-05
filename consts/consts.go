9a69f219891f6241305eddc90265001e741332c9
maryonyinye77 <tylerblark7@gmail.com>
2024-07-05 00:35:31 +0000

    maryann

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "mary"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "onyi"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "maryann"
 )
 
 var ID ids.ID
